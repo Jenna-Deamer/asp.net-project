549496057cd060640ce047fe49ea22d696cf0e20
Jenna Deamer <jennadeamer@gmail.com>
2024-01-12 19:13:08 -0500

    first commit basic file setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace newAspProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
